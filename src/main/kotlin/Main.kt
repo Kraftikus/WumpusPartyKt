8346eefe88fa116b4df68d42e64e252dc5b45a43
Daniel K <45011108+Kraftikus@users.noreply.github.com>
2022-12-02 15:47:10 +0100

    Init Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+import dev.kord.core.Kord
+import dev.kord.core.entity.ReactionEmoji
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+import kotlinx.coroutines.delay
+
+suspend fun main() {
+    val token = ""
+
+    val kord = Kord(token)
+    val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
+
+    kord.on<MessageCreateEvent> {
+        if (message.content != "!ping") return@on
+
+        val response = message.channel.createMessage("Pong!")
+        response.addReaction(pingPong)
+
+        delay(5000)
+        message.delete()
+        response.delete()
+    }
+
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
+}
